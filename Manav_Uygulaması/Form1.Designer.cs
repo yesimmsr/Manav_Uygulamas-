94df06b85083838680c4eed7edaea42a127cfc66
yesimmsr <misiryesim@gmail.com>
2021-12-29 20:17:54 +0300

    Add project files.

---
+++ Manav_Uygulaması/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Manav_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstSepet = new System.Windows.Forms.ListBox();
+            this.cmbUrun = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSiparisiBitir = new System.Windows.Forms.Button();
+            this.lblOzet = new System.Windows.Forms.Label();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstSepet
+            // 
+            this.lstSepet.FormattingEnabled = true;
+            this.lstSepet.ItemHeight = 17;
+            this.lstSepet.Location = new System.Drawing.Point(12, 171);
+            this.lstSepet.Name = "lstSepet";
+            this.lstSepet.Size = new System.Drawing.Size(189, 225);
+            this.lstSepet.TabIndex = 0;
+            // 
+            // cmbUrun
+            // 
+            this.cmbUrun.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUrun.FormattingEnabled = true;
+            this.cmbUrun.Location = new System.Drawing.Point(56, 12);
+            this.cmbUrun.Name = "cmbUrun";
+            this.cmbUrun.Size = new System.Drawing.Size(133, 25);
+            this.cmbUrun.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürün";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(55, 73);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fiyat";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(55, 104);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSiparisiBitir
+            // 
+            this.btnSiparisiBitir.Location = new System.Drawing.Point(113, 402);
+            this.btnSiparisiBitir.Name = "btnSiparisiBitir";
+            this.btnSiparisiBitir.Size = new System.Drawing.Size(88, 31);
+            this.btnSiparisiBitir.TabIndex = 6;
+            this.btnSiparisiBitir.Text = "Siparişi Bitir";
+            this.btnSiparisiBitir.UseVisualStyleBackColor = true;
+            this.btnSiparisiBitir.Click += new System.EventHandler(this.btnSiparisiBitir_Click);
+            // 
+            // lblOzet
+            // 
+            this.lblOzet.AutoSize = true;
+            this.lblOzet.ForeColor = System.Drawing.Color.Red;
+            this.lblOzet.Location = new System.Drawing.Point(16, 151);
+            this.lblOzet.Name = "lblOzet";
+            this.lblOzet.Size = new System.Drawing.Size(173, 17);
+            this.lblOzet.TabIndex = 7;
+            this.lblOzet.Text = "Toplam 13 adet Ürün 150 TL";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(12, 402);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 31);
+            this.btnSil.TabIndex = 9;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Kg";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(56, 42);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 458);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.lblOzet);
+            this.Controls.Add(this.btnSiparisiBitir);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbUrun);
+            this.Controls.Add(this.lstSepet);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manav";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstSepet;
+        private System.Windows.Forms.ComboBox cmbUrun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSiparisiBitir;
+        private System.Windows.Forms.Label lblOzet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Manav_Uygulaması/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manav_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] urunler = { "Domates", "Patlıcan", "Kabak", "Biber", "Elma", "Armut", "Üzüm", "Salatalık", "Marul" };
+        decimal[] fiyatlar = new decimal[0];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Form ekranı gelirken bu event tetiklenir.
+            FormFill();
+
+        }
+
+        void FormFill()
+        {//Form açıldığında combobox dolu gelsin diye
+            //ürünler doldurulur.
+
+            for (int i = 0; i < urunler.Length; i++)
+            {
+                cmbUrun.Items.Add(urunler[i]);
+            }
+        }
+
+        void FormClear()
+        {
+            cmbUrun.SelectedIndex = 0;
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 0;
+        }
+
+        void fiyatEkle(decimal toplamFiyat)
+        {
+            Array.Resize(ref fiyatlar, fiyatlar.Length + 1;
+            fiyatlar[fiyatlar.Length - 1] = toplamFiyat;
+
+        }
+
+        Decimal toplamFiyatlar()
+        {
+            decimal toplamFiyat = 0;
+            for(int i=0; i<fiyatlar.Length; i++)
+            {
+                toplamFiyat += toplamFiyatlar
+            }
+        }
+
+        void toplamLabelGuncelle()
+        {
+            int toplamKg = fiyatlar.Length;
+            decimal toplamFiyat =
+            lblOzet.Text = $"Toplam {toplamKg} adet ürün {toplamFiyat} TL";
+        }
+
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+
+            if (cmbUrun.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen bir ürün seçiniz");
+                cmbUrun.Focus();//cmb seçili hale getirir.
+                return; //Kodu burada keser aşağıya devam etmez.
+            }
+
+            string urunAdi = cmbUrun.SelectedIndex.ToString();
+
+            decimal fiyat = numericUpDown1.Value;
+            decimal kg = numericUpDown2.Value;
+            decimal toplamFiyat = fiyat * kg;
+
+            string eklenecekUrun = $"{urunAdi} {fiyat}*{kg}={toplamFiyat}TL ";
+            //Toplamfiyat ayrı bir dizide tutuyorum.
+            FiyatlariEkle(toplamFiyat);
+            lstSepet.Items.Add(eklenecekUrun);
+            toplamLabelGuncelle();
+
+            FormClear(); //formu temizler
+            cmbUrun.Focus(); //Ürüne fokusları kullanı
+
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            //int secim = lstSepet.SelectedIndex;
+            //if (secim != -1)
+            //{
+            //    lstSepet.Items.RemoveAt(secim);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Silinecek ürünü seçiniz.");
+            //}
+
+            DialogResult result = MessageBox.Show("Seçilen ürünü silmek istiyor musunuz?", "Manav", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (result == DialogResult.OK)
+            {
+                lstSepet.Items.RemoveAt();
+            }
+            else
+            {
+
+            }
+
+        }
+
+        private void btnSiparisiBitir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lblToplamFiyat.Text);
+            FormClear();
+            lstSepet.Items.Clear();
+            fiyatlar = new decimal[0];
+        }
+    }
+}
